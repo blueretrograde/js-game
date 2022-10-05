4a4592eebcb3d43a7c9930e9d6e31f0f2d6666e6
Nicholas <blueretrograde@gmail.com>
2022-10-05 16:28:45 +0100

    added some basic HTML just to have a structure

---
+++ script.js
@@ -0,0 +1,7 @@
+//an array of questions with each question containing an array of answers
+
+//click events for Start Game, Next, Restart(at the end of game)
+
+//functions for Correct Answer, Wrong answer, Win, Loss
+
+//function to reset the gamme 
