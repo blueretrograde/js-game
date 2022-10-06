0c2ef77d94f30273312a465ded53a4ec77b29ef4
Nicholas <blueretrograde@gmail.com>
2022-10-06 15:16:57 +0100

    just did the firs bit of pseudocode in my JS file

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+//accessing my components
+
+
+
 //an array of questions with each question containing an array of answers
+//(different js file for that??)
+
 
 //click events for Start Game, Next, Restart(at the end of game)
 
+
+
 //functions for Correct Answer, Wrong answer, Win, Loss
 
-//function to reset the gamme 
+
+
+//function to reset the game 
+
+
+
+
+//function to proceed to the next question
