401a17c4357bd955bdfdfd2f71e9f6c9e29ec58a
Nicholas <blueretrograde@gmail.com>
2022-10-07 12:33:24 +0100

    done my document selectors in JS

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-//accessing my components
+//ACESSING MY COMPONENTS
+
+    //Game title
+    const title = document.querySelector(".game__title")
+    //Main Background(Display & Answer buttons)
+    const gameBackground = document.querySelector(".game__background")
+    //Display (Question Display Screen)
+    const questionDisplay = document.querySelector(".game__display")
+    //The actual question text
+    const gameQuestions = document.querySelector(".game__questions")
+    //The answer buttons & Next and Start buttons (the whole div)
+    const gameButtons = document.querySelector(".game__buttons")
+    //An "All Selector" for the four answer buttons
+    const gameButton = document.querySelectorAll(".game__button")
+    //The Next Question Button
+    const nextQuestion = document.querySelector(".game__button--nextquestion")
+    //The Start/Restart button
+    const startButton = document.querySelector(".game__button--start")
 
 
+//AN OBJECT OF QUESTIONS WITH EACH ITEM CONTATINING AN ARRAY OF 4 ANSWERS
+//(different js file for that)
 
-//an array of questions with each question containing an array of answers
-//(different js file for that??)
+    //@import here
+
+
 
 
 //click events for Start Game, Next, Restart(at the end of game)
